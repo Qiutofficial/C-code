27c1d9c7035e9c6114b8ba6696aa5995d8f06827
Qiutofficial <31537763+Qiutofficial@users.noreply.github.com>
2017-10-27 20:17:29 +0300

    Update Lab1.c

--- Lab1.c
+++ Lab1.c
@@ -1,26 +1,34 @@
-#define _CRT_SECURE_NO_WARNINGS
-
 #include <stdio.h>
 
-int main()
+void Error()
+{
+	printf("Error");
+}
+
+int main(int argc, char* argv[])
 {
 	char t;
 	float z;
-	
-	printf("Vvedite znachenie: ");
-	scanf("%f", &z);
-	printf("Ukazhite tip temperaturi: ");
-	getchar();
-	t = getchar();
-
-	if (t == 'C' || t == 'c') printf("\nC = %f\nK = %f\nF = %f", z, z + 273.15, z * 1.8 + 32);
-	else
+	if (argc < 2) { Error(); return 0; }
+	if (argc == 2) 
+	{ 
+		sscanf(argv[1], "%f", &z);
+		printf("\nC = %.2f\nK = %.2f\nF = %.2f\n\nC = %.2f\nK = %.2f\nF = %.2f\n\nC = %.2f\nK = %.2f\nF = %.2f", 
+		z, z + 273.15, z * 1.8 + 32, z - 273.15, (z - 273.15) * 1.8 + 32, z, (z - 32) / 1.8, z, (z - 32) / 1.8 - 273.15);
+	}
+	if (argc == 3) 
 	{
-		if (t == 'F' || t == 'f') printf("\nC = %f\nK = %f\nF = %f", z - 273.15, (z - 273.15) * 1.8 + 32, z);
+		t = argv[2][0];
+		sscanf(argv[1], "%f", &z);
+		if (t == 'C' || t == 'c') printf("\nC = %.2f\nK = %.2f\nF = %.2f", z, z + 273.15, z * 1.8 + 32);
 		else
 		{
-			if (t == 'K' || t == 'k') printf("\nC = %f\nK = %f\nF = %f", (z - 32) / 1.8, z, (z - 32) / 1.8 - 273.15);
+			if (t == 'F' || t == 'f') printf("\nC = %.2f\nK = %.2f\nF = %.2f", z - 273.15, (z - 273.15) * 1.8 + 32, z);
+			else
+			{
+				if (t == 'K' || t == 'k') printf("\nC = %.2f\nK = %.2f\nF = %.2f", (z - 32) / 1.8, z, (z - 32) / 1.8 - 273.15);
+			}
 		}
 	}
-	_getch();
+	return 0;	
 }
